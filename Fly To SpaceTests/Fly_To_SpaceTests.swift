84aeb8b2d19b2d4b7ac586fafd40ab4500de179e
White Acre Software <tom@tomw.net>
2025-11-25 17:20:12 -0600

    Initial Commit

---
+++ Fly To SpaceTests/Fly_To_SpaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fly_To_SpaceTests.swift
+//  Fly To SpaceTests
+//
+//  Created by tom whittaker on 11/25/25.
+//
+
+import Testing
+@testable import Fly_To_Space
+
+struct Fly_To_SpaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
